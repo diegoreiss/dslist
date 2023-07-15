1e3d1a79d355146eba65490bdede3482e7d5b404
diegoreiss <diegoportal.reis@outlook.com>
2023-07-15 00:19:05 -0300

    add GameController com funcao findAll

---
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.service.GameService;
+
+@RestController
+@RequestMapping(value = "/game")
+public class GameController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
