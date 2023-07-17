4f672b70c1e1ba0283a37cdd0cbefc8049085cff
diegoreiss <diegoportal.reis@outlook.com>
2023-07-17 06:16:10 -0300

    tratando erro no controller de id inexistente

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -3,13 +3,16 @@ package com.devsuperior.dslist.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.exception.GameNotFoundException;
 import com.devsuperior.dslist.service.GameService;
 
 @RestController
@@ -25,6 +28,10 @@ public class GameController {
 
     @GetMapping(value = "/{id}")
     public GameDTO findById(@PathVariable long id) {
-        return gameService.findById(id);
+        try {
+            return gameService.findById(id);
+        } catch (GameNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
     }
 }
