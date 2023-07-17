e82954bda363a881c20e5beaa96310ef339d54c5
diegoreiss <diegoportal.reis@outlook.com>
2023-07-17 01:56:03 -0300

    retirando annotation @Data e inserindo @Gettter/@Setter e @EqualsAndHashCode

--- src/main/java/com/devsuperior/dslist/entitiy/Game.java
+++ src/main/java/com/devsuperior/dslist/entitiy/Game.java
@@ -7,12 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "game")
 public class Game {
@@ -20,12 +24,10 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
 
     @Column(name = "game_year")
     private Integer year;
-
     private String genre;
     private String platform;
     private Double score;
